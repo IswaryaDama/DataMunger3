9a2c7cfa9100acec6477ba45533690b11ad68378
ubuntu <ubuntu@ip-172-31-83-26.ec2.internal>
2019-11-07 10:55:49 +0000

    Data Munger3 result

--- PROBLEM.md
+++ PROBLEM.md
@@ -4,3 +4,54 @@ In this Assignment 3, we will
 
     1. Read the header.  
     2. Identify the Data type of each field 
+   
+  ******************************************************************************* 
+    
+    
+   ******************************************************************************** 
+    Object obj;
+		String[] dataTypes = null;
+		String[] dataTypeElements =null;
+		
+		while((dataType = csvRead.readLine())!=null) {
+        	dataTypes = dataType.split(",");
+        	dataTypeElements = new String[dataTypes.length];
+        	 System.out.println(" data in file" + dataType);
+        	 for(int i=0;i<dataTypes.length;i++) {
+        		 
+        			 try {
+        				obj = Integer.parseInt(dataTypes[i]);
+        				if(obj instanceof Integer)
+        					dataTypeElements[i] = obj.getClass().getName();
+        			 }catch(NumberFormatException e) {
+        				 try{
+        					 obj = Double.parseDouble(headerList[i]);
+        					 if(obj instanceof Double) {
+        						 dataTypeElements[i] = obj.getClass().getName();
+        					 }
+        					 
+        				 }catch(Exception ex) {
+        					 
+        					 obj = headerList[i];
+        					 if(obj instanceof String) {
+        						 dataTypeElements[i] = obj.getClass().getName();
+        					 }
+        				 }
+        			 }
+        			 finally {
+        				 
+        			 }
+        			 
+        		 
+        	 }
+         }
+		csvRead.close();
+		DataTypeDefinitions typeDef = new DataTypeDefinitions(dataTypeElements);
+		return typeDef;
+	}
+	
+	
+	
+	
+	
+	
--- src/com/stackroute/datamunger/reader/CsvQueryProcessor.java
+++ src/com/stackroute/datamunger/reader/CsvQueryProcessor.java
@@ -1,16 +1,26 @@
 package com.stackroute.datamunger.reader;
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 
 import com.stackroute.datamunger.query.DataTypeDefinitions;
 import com.stackroute.datamunger.query.Header;
 
 public class CsvQueryProcessor extends QueryProcessingEngine {
+	
+	String fileName;
+	BufferedReader csvRead = null;
+	Header headers = null;
+	String dataType;
+	String[] headerList;
 
 	// Parameterized constructor to initialize filename
 	public CsvQueryProcessor(String fileName) throws FileNotFoundException {
-
+            this.fileName = fileName;
+            csvRead = new BufferedReader(new FileReader(fileName));
 	}
 
 	/*
@@ -21,12 +31,23 @@ public class CsvQueryProcessor extends QueryProcessingEngine {
 	
 	@Override
 	public Header getHeader() throws IOException {
-
+		
 		// read the first line
+		csvRead.mark(1);
+		String firstLine = csvRead.readLine();
+		System.out.println("headrers list " + Arrays.toString(firstLine.split(",")));
+		headerList = firstLine.split(",");
+		csvRead.reset();
+		System.out.println("header size" + headerList.length);
+		 headers = new Header(headerList);
 
 		// populate the header object with the String array containing the header names
-		return null;
+		return headers;
+	
+		
+		
 	}
+	
 
 	/**
 	 * getDataRow() method will be used in the upcoming assignments
@@ -49,7 +70,47 @@ public class CsvQueryProcessor extends QueryProcessingEngine {
 	
 	@Override
 	public DataTypeDefinitions getColumnType() throws IOException {
+		Object obj;
+		int i = 0;
+		String type = "";
+		BufferedReader br = new BufferedReader(new FileReader(fileName));
+		String headerRow = br.readLine();
+		String headerArgs[] = headerRow.split(",");
+		String secondRow = br.readLine();
+		secondRow += " ,";
+		String[] dataElements = secondRow.split(",");
+		String[] dataTypes = new String[headerArgs.length];
+		if (dataElements != null) {
+			for (i = 0; i < dataElements.length; i++) {
 
-		return null;
-	}
+				try {
+					obj = Integer.parseInt(dataElements[i]);
+					if (obj instanceof Integer)
+
+						type = obj.getClass().getName();
+					dataTypes[i] = type;
+				} catch (NumberFormatException e) {
+
+					try {
+						obj = Double.parseDouble(dataElements[i]);
+						if (obj instanceof Double) {
+							type = obj.getClass().getName();
+							dataTypes[i] = type;
+						}
+					} catch (Exception e2) {
+						obj = dataElements[i];
+						if (obj instanceof String)
+							type = obj.getClass().getName();
+						dataTypes[i] = type;
+					}
+				}
+			}
+
+		}
+
+		br.close();   
+		
+		DataTypeDefinitions types = new DataTypeDefinitions(dataTypes);
+		return types;
+	}	
 }
